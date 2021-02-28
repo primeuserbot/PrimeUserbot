72b096335d3cb4dc732a3dc25772856c17224a99
primeuserbot <79791986+primeuserbot@users.noreply.github.com>
2021-02-28 23:07:56 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,4 +1,4 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+"""Check if userbot alive. If you change these, you are noob."""
 #IMG CREDITS: @CallPrime
 import asyncio
 from telethon import events
@@ -7,7 +7,7 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/30f297984523fccb8ce8c.jpg"
-pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
+pm_caption = "`PrimeUserbot IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **1.15.0**\n`Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
